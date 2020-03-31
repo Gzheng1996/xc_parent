34a7bb96a9d5a0128c1ad01ce4c2681359bcdde5
guan <2702788640@qq.com>
2020-03-31 16:27:27 +0800

    gz

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -29,6 +29,10 @@ public class ManageCmsApplication {
 
         System.out.println("搞毛线+sss");
         System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss");
+        System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss");
+        System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss");
+        System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss");
+        System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss"); System.out.println("搞毛线+sss");
 
         return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
 
